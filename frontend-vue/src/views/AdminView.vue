be48b2ab19623b4fb4ac2c6d6d007a0b1ab06e22
derick321 <derick321@kaist.ac.kr>
2023-06-05 20:05:44 +0900

    Implement AdminView and related vuex store functions

--- frontend-vue/src/views/AdminView.vue
+++ frontend-vue/src/views/AdminView.vue
@@ -3,8 +3,72 @@
     <div class="t-title">
       AdminView
     </div>
-    <div>
-      ToFill: AdminView Inners
+    <div>CURRENT CLUBS
+      <div v-for="(club, id) in currentClubs" v-bind:key="id">
+        {{ club.name }}
+        <span @click="deleteClub(id)">
+          delete
+        </span>
+      </div>
+    </div>
+    <div>REQUEST FOR NEW CLUBS
+      <div v-for="(request, id) in requestsNewClub" v-bind:key="id">
+        {{ request.clubName }}
+        <div>Requester: {{ request.requestUser }}</div>
+        <div>Description: {{ request.clubDescription }}</div>
+        <span @click="denytNewClub(id)">
+          deny
+        </span>
+        <span @click="accepttNewClub(id)">
+          approve
+        </span>
+      </div>
+    </div>
+    <div>REQUEST FOR HANDOVER
+      <div v-for="(club, id) in requestsHandover" v-bind:key="id">  
+        {{ club.name }}
+        <div>
+          <span>{{ request.fromUserId }}</span>
+          <span> -> </span>
+          <span>{{ request.toUserId }}</span>
+        </div>
+        <span @click="denyHandover(id)">
+          deny
+        </span>
+        <span @click="acceptHandover(id)">
+          approve
+        </span>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from "vuex"
+
+export default {
+  name: "AdminView",
+  components: {
+  },
+  computed: {
+    ...mapGetters({
+      currentClubs: "currentClubs",
+      requestsNewClub: "requestsNewClub",
+      requestsHandover: "requestsHandover",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      fetchAdminInfo: "fetchAdminInfo",
+      deleteClub: "deleteClub",
+      acceptNewClub: "acceptNewClub",
+      denyNewClub: "denyNewClub",
+      acceptHandover: "acceptHandover",
+      denyHandover: "denyHandover",
+    }),
+  },
+  beforeMount() {
+    this.fetchAdminInfo()
+  },
+}
+</script>
